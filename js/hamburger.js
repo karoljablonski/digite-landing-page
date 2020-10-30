d5792e64b35c7025b23a4e199b31d78b600301d8
Karol Jabłoński <karoljablonski373@gmail.com>
2020-10-30 12:09:53 +0100

    change nav bar bgc, js scipt bgc, style h1 and h2 "content"

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,4 +2,11 @@ $("span.burger").on("click", function() {
     $("i.fa-bars, i.fa-times").toggleClass("turned-off")
     $(".main-nav-ul").toggleClass("active-hamburger")
 });
-
+const navigation = document.querySelector(".main-nav");
+addEventListener("scroll", function() {
+    if(scrollY > 230) {
+        navigation.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
+    } else {
+        navigation.style.backgroundColor = "rgba(255, 255, 255, 0.2)"
+    };
+});
